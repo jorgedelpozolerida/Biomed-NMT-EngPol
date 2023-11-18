ba175da6503da0fc6f0d1ff809d8e5df17372db1
jorgedelpozolerida <jorgedelpozolerida@gmail.com>
2023-11-18 16:56:16 +0100

    created scripts for dataset generation, training and evaluation (almost finished)

---
+++ src/evaluate.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+""" Script to evaluate mBART50 model on test data
+
+This script does the following:
+- loads tokenized dataset and trained model
+- calculates BLEU and sacreBLEU metrics
+
+Note on folder structure:
+└── args.base_dir
+    ├── data
+    │   
+    ├── filtering
+    │   
+    ├── logs
+    │   
+    ├── models
+    │   ├── *submodel_1*
+    │   ├── *submodel_2*
+    │   ...
+    │   └── *submodel_N*
+    ├── tokenizers
+    │   └── *dataset_tokenized*
+    └── training
+    
+
+@author: jorgedelpozolerida
+@date: 17/11/2023
+"""
+
+import os
+import sys
+import argparse
+import logging
+import datasets
+from sklearn.metrics import accuracy_score
+from transformers import MBart50Tokenizer, MBartForConditionalGeneration
+import sacrebleu
+from nltk.translate.bleu_score import corpus_bleu
+from datasets import load_metric
+
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger(__name__)
+
+def load_tokenized_dataset(base_dir, dataset_name):
+    tokenizer_folder = os.path.join(base_dir, "tokenizers")
+    return datasets.load_from_disk(os.path.join(tokenizer_folder, dataset_name))
+
+def load_local_model(base_dir, model_name):
+    models_folder = os.path.join(base_dir, "models")
+    return MBartForConditionalGeneration.from_pretrained(os.path.join(models_folder, model_name))
+
+def evaluate_model(model, tokenizer, test_dataset, source_lang, target_lang):
+    model.eval()
+    preds, labels = [], []
+
+    for example in test_dataset:
+        input_ids = tokenizer.encode(example[source_lang], return_tensors='pt')
+        outputs = model.generate(input_ids)
+        pred = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        label = example[target_lang]
+
+        preds.append(pred)
+        labels.append([label])  # Note: labels need to be a list of lists for nltk's corpus_bleu
+
+    # Compute SacreBLEU
+    sacrebleu_metric = load_metric('sacrebleu')
+    sacrebleu_results = sacrebleu_metric.compute(predictions=preds, references=labels)
+    sacrebleu_score = sacrebleu_results["score"]
+
+    # Compute NLTK BLEU
+    nltk_bleu_score = corpus_bleu(labels, preds)
+
+    return sacrebleu_score, nltk_bleu_score
+
+def main(args):
+    
+    assert os.path.isdir(args.base_dir)
+    assert args.source_lang != args.target_lang
+    
+    model_name = "facebook/mbart-large-50-one-to-many-mmt"
+
+    # Load tokenized dataset
+    tokenized_datasets = load_tokenized_dataset(args.base_dir, args.dataset_name)
+    test_dataset = tokenized_datasets['test']
+
+    # Load tokenizer
+    tokenizer = MBart50Tokenizer.from_pretrained(model_name)
+
+    models_folder = os.path.join(args.base_dir, "models")
+    assert os.path.isdir(models_folder)
+
+    # Evaluate pretrained model
+    pretrained_model = MBartForConditionalGeneration.from_pretrained(model_name)
+    sacrebleu_score, nltk_bleu_score = evaluate_model(pretrained_model, tokenizer, test_dataset, args.source_lang, args.target_lang)
+    _logger.info(f'Pretrained Model\n' + f'\tSacreBLEU: {sacrebleu_score}\n' + f'\tNLTK BLEU: {nltk_bleu_score}\n')
+    
+    
+    # Loop over models in the models directory
+    for model_name in os.listdir(models_folder):
+        if args.submodel_name is not None and model_name != args.submodel_name:
+            continue
+        msg = ''
+        model_path = os.path.join(models_folder, model_name)
+        if os.path.isdir(model_path):
+            msg += f"Model: {model_name}\n"
+            model = load_local_model(args.base_dir, model_path)
+            sacrebleu_score, nltk_bleu_score = evaluate_model(model, tokenizer, test_dataset, args.source_lang, args.target_lang)
+            msg +=f'\tSacreBLEU: {sacrebleu_score}\n'
+            msg += f'\tNLTK BLEU: {nltk_bleu_score}'
+            _logger.info(msg)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Evaluate mBART50 Model")
+
+    parser.add_argument('--base_dir', type=str, required=True, help='Base directory for the project')
+    parser.add_argument('--submodel_name', type=str,  help='Name of the single fine-tuned model inside models/ folder')
+    parser.add_argument('--dataset_name', type=str, default="dataset_tokenized", help='Name of the tokenized dataset')
+    parser.add_argument('--source_lang', type=str, default="pol", choices=['eng', 'pol'], help='Source language')
+    parser.add_argument('--target_lang', type=str, default="eng", choices=['eng', 'pol'], help='Target language')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
---
+++ src/train.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+""" Script to train mBART50 on filtered data
+
+
+This script does the following:
+- loads tokenized dataset
+- performs filtering based on method and level form args
+- generates train-val split for fitlered data
+- fine-tunes model
+- saves model
+
+TODO:
+- agree on some nice hyperparaemters for trianing
+- agree on layers to freeze
+
+
+
+@author: jorgedelpozolerida
+@date: 17/11/2023
+"""
+
+import os
+import sys
+import argparse
+import traceback
+
+from typing import Dict, Any
+import logging                                                                      # NOQA E402
+import numpy as np                                                                  # NOQA E402
+import pandas as pd                                                                 # NOQA E402
+import os
+import sys
+import torch
+import gc
+import datasets
+from sklearn.model_selection import train_test_split
+
+from transformers import MBart50Tokenizer, MBartForConditionalGeneration, TrainingArguments, Trainer
+
+
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger(__name__)
+
+# Mapping of column names for languages to mBART50 language codes
+MAPPING_LANG: Dict[str, str] = {
+    "pol": "pl_PL",
+    "eng": "en_XX"
+}
+
+def filter_and_split_dataset(args, original_dataset):
+    """
+    Filters and splits a given dataset based on a filter file and stratifies the split.
+
+    Args:
+        args: ArgumentParser object containing necessary configurations.
+        original_dataset: The original dataset to be filtered and split.
+    """
+    
+    # Create a deep copy of the original dataset to avoid modifying it directly
+    dataset = original_dataset.copy()
+
+    # Attempt to read the filter file with comma or tab separator
+    try:
+        ids_selected = pd.read_csv(os.path.join(args.base_dir, "filtering", args.filter_file), sep=',')
+    except pd.errors.ParserError:
+        ids_selected = pd.read_csv(os.path.join(args.base_dir, "filtering", args.filter_file), sep='\t')
+
+    # Convert 'id' column from the filter file into a set for efficient searching
+    selected_ids_set = set(ids_selected['id'])
+
+    # Filter the 'train_val' split of the dataset
+    train_val_dataset = dataset['train_val'].filter(lambda example: example['id'] in selected_ids_set)
+
+    # Stratified split of the filtered 'train_val' dataset
+    train_val_split = train_val_dataset.train_test_split(test_size=0.2, stratify_by_column='src')
+
+    # Update the dataset with new 'train' and 'val' splits
+    dataset['train'] = train_val_split['train']
+    dataset['val'] = train_val_split['test']
+
+    # Remove the 'train_val' split
+    del dataset['train_val']
+
+    return dataset
+
+def get_run_metadata(args):
+    '''
+    Function to extarct metadata from filtering file for run
+    '''
+    
+    if args.filter_file is None:
+        return None, None, "Baseline"
+    
+    filename_split = args.filter_file.split(".")[0].split("_")
+    if len(filename_split) < 2:
+        raise ValueError(f"Invalid format for 'filter_file': {args.filter_file}")
+    
+    method = filename_split[0]
+    level = filename_split[-1]
+
+    # Create a subname by combining 'method' and 'level'
+    subname = f"{method}_{level}"
+
+    return method, level, subname
+
+def main(args):
+
+    assert os.path.isdir(args.base_dir)
+    assert args.source_lang != args.target_lang
+
+    model_name = "facebook/mbart-large-50-one-to-many-mmt"
+
+    # Setup directories
+    models_folder = os.path.join(args.base_dir, "models")
+    tokenizer_folder = os.path.join(args.base_dir, "tokenizers")
+    training_folder = os.path.join(args.base_dir, "training")
+    logs_folder = os.path.join(args.base_dir, "logs")
+    _logger.info(f"Directories set up: models_folder={models_folder}, tokenizer_folder={tokenizer_folder}, results_folder={training_folder}, logs_folder={logs_folder}")
+
+    # Log all parameters
+    _logger.info(f"Parameters: {args}")
+
+    method, level, model_subname = get_run_metadata(args)
+    
+    # Use a GPU if available and specified
+    if "gpu" in args.device_name.lower():
+        device = torch.device(f"cuda:{args.device_name}" if torch.cuda.is_available() else "cpu")
+    else:
+        device = "cpu"
+    _logger.info(f"Using device: {device}")
+    
+    # Load tokenizer and tokenized dataset
+    tokenizer = MBart50Tokenizer.from_pretrained(model_name, 
+                                                    src_lang=MAPPING_LANG[args.source_lang], 
+                                                    tgt_lang=MAPPING_LANG[args.target_lang]
+                                                    )  
+    tokenized_datasets = datasets.load_from_disk(os.path.join(tokenizer_folder, args.dataset_name))
+
+
+    # Perform filtering
+    if args.filter_file is not None:
+        tokenized_datasets_filt = filter_and_split_dataset(args, tokenized_datasets)
+    else:
+        tokenized_datasets_filt = tokenized_datasets.copy()
+    
+    
+    # Logging
+    train_size = len(tokenized_datasets_filt['train'])
+    validation_size = len(tokenized_datasets_filt['val'])
+    test_size = len(tokenized_datasets_filt['test'])
+    total_size_after_filtering = train_size + validation_size
+    total_size_before_filtering = len(tokenized_datasets['train_val'])
+
+    _logger.info(
+        f"Finished filtering dataset '{args.dataset_name}' using {args.filter_file}\n" + \
+        f"\tSize before filtering: {total_size_before_filtering}\n" + \
+        f"\tSize after filtering: {total_size_after_filtering}\n" + \
+        f"\ttrain_size: {train_size}, valid_size: {validation_size}, test_size: {test_size}"
+    )
+
+
+    # Load model
+    model = MBartForConditionalGeneration.from_pretrained(model_name)
+
+    # Freeze all layers except the last one. TODO: investigate best layers to freeze
+    msg = 'Unfreezing layers: \n'
+    for name, param in model.named_parameters():
+        if 'model.encoder.layers.11.' in name or 'model.decoder.layers.11.' in name:
+            msg += f"\t{name}\n"
+            param.requires_grad = True
+        else:
+            param.requires_grad = False
+    _logger.info(msg)
+    
+    # Define your training arguments
+    training_args = TrainingArguments(
+        output_dir=training_folder,          # Output directory for model checkpoints
+        num_train_epochs=args.num_train_epochs,                 # Number of training epochs
+        per_device_train_batch_size=args.train_batch_size,      # Batch size for training (small for GPU memory issues)
+        per_device_eval_batch_size=args.eval_batch_size,       # Batch size for evaluation
+        warmup_steps=500,                   # Number of warmup steps
+        weight_decay=0.01,                  # Weight decay if applicable
+        logging_dir=os.path.join(logs_folder, model_subname, ),            # Directory for logs
+        save_strategy='epoch',              # Save strategy
+        evaluation_strategy='steps'         # Evaluation strategy
+    )
+
+
+    # Initialize Trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_datasets_filt['train'],
+        eval_dataset=tokenized_datasets_filt['val'],
+        tokenizer=tokenizer
+    )
+
+    # Training
+    _logger.info("Starting training")
+    trainer.train()
+
+    # Save model
+    trainer.save_model(os.path.join(models_folder, model_subname))
+
+
+
+def parse_args():
+    '''
+    Parses all script arguments.
+    '''
+    parser = argparse.ArgumentParser()
+
+
+    parser.add_argument('--base_dir', type=str, default=None, required=True,
+                        help='Path where all subfolders are present for the project')
+    parser.add_argument('--device_name', type=str, default="cpu", 
+                        help='Name of device to use')
+    parser.add_argument('--dataset_name', type=str, default="dataset_tokenized",
+                        help='Name of device to use')
+    parser.add_argument('--filter_file', type=str, default=None,
+                        help='Name of file with ids after filtering')
+    parser.add_argument('--source_lang', type=str, default="pol", choices=['eng', 'pol'],
+                        help='Source language')
+    parser.add_argument('--target_lang', type=str, default="eng", choices=['eng', 'pol'],
+                        help='Target language')
+    parser.add_argument("--num_train_epochs", type=int, default=3, help="Number of training epochs")
+    parser.add_argument("--train_batch_size", type=int, default=2, help="Batch size for training")
+    parser.add_argument("--eval_batch_size", type=int, default=2, help="Batch size for evaluation")
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)

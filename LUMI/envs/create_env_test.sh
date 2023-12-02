7b7969114e128014ea98defc46f330ad3b861f57
jorgedelpozolerida <jorgedelpozolerida@gmail.com>
2023-12-02 21:04:25 +0200

    envs, containers and more

---
+++ LUMI/envs/create_env_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --job-name=examplejob   # Job name
+#SBATCH --output=logs/examplejob_output_%j # Name of stdout output file
+#SBATCH --error=logs/examplejob_error_%j  # Name of stderr error file
+#SBATCH --partition=standard-g  # Partition (queue) name
+#SBATCH --account=project_465000872  # Project for billing
+#SBATCH --time=0-02:00:00       # Run time (d-hh:mm:ss)
+
+module load LUMI/22.08
+# module load LUMI/22.08 partition/G
+module load cotainr
+cotainr build test_container_v2.sif  --base-image=docker://rocm/dev-ubuntu-22.04:5.3.2-complete --conda-env=test-env.yml

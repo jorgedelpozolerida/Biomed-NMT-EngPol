25a18959f711fc120e455fbf40518e3d02e7020a
jorgedelpozolerida <jorgedelpozolerida@gmail.com>
2023-12-02 21:34:41 +0200

    added scaffold jobs for train, tokenize

---
+++ LUMI/jobs/tokenize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --job-name=tokenize   # Job name
+#SBATCH --output=../logs/tokenize.o%j # Name of stdout output file
+#SBATCH --error=../logs/tokenize.e%j  # Name of stderr error file
+#SBATCH --partition=standard-g  # Partition (queue) name
+#SBATCH --nodes=1               # Total number of nodes
+#SBATCH --ntasks-per-node=8     # 8 MPI ranks per node, 128 total (16x8)
+#SBATCH --gpus-per-node=1       # Allocate one gpu per MPI rank
+#SBATCH --time=0-02:00:00       # Run time (d-hh:mm:ss)
+#SBATCH --account=project_465000872  # Project for billing
+
+singularity exec -B /scratch/project_465000872 ../envs/test_container.sif python3 ../src/tokenize_dataset.py --base_dir /scratch/project_465000872/DATA

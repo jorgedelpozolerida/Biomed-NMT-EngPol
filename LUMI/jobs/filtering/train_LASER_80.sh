6aafcd52cb8f9411363eafa513207df925787750
jorgedelpozolerida <jorgedelpozolerida@gmail.com>
2023-12-08 14:20:10 +0200

    restore data

---
+++ LUMI/jobs/filtering/train_LASER_80.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --job-name=train   # Job name
+#SBATCH --output=../../logs/train/filt/%j.output # Name of stdout output file
+#SBATCH --error=../../logs/train/filt/%j.error  # Name of stderr error file
+#SBATCH --partition=standard-g  # Partition (queue) name
+#SBATCH --nodes=1               # Total number of nodes
+#SBATCH --gpus-per-node=1       # Allocate one gpu per MPI rank
+#SBATCH --time=0-05:00:00       # Run time (d-hh:mm:ss)
+#SBATCH --account=project_465000872  # Project for billing
+
+singularity exec -B /scratch/project_465000872 ../../envs/test_container_v3.sif \
+    python3 ../../src/train.py \
+        --base_dir /scratch/project_465000872/DATA \
+        --config_path ../../src/train_config.json \
+        --seed 42 \
+        --filter_file LASER_above_quantile_80.tsv
